ae4e0c818a437a5ab9d056519c391b611d252a95
mfec <mfec@192.168.1.40>
2017-03-11 07:18:39 +0700

    bot update 110317

--- bot.php
+++ bot.php
@@ -1,8 +1,13 @@
 <?php 
 require_once __DIR__ . "/vendor/autoload.php";
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient;
+use LINE\LINEBot\Event;
+use LINE\LINEBot\MessageBuilder;
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('nCXaaQP4IrDgdM2G/gf1fc6eA1eo1R5VL0MiaZyvjEoqZNevPHUHYOvKXNDNCLz5q1IvBMHPFoCId/k0kjR7Gkql411JhfX7IsLueFb3XlI5dvEshaZYIrpKpmsoxt651wSWjwO5rmTRGW4NC/QSkQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'f9be14bacc7ac4f857ca6c3fb0f543ef']);
-$event = new LINE\LINEBot\Event('user');
+$event = new \LINE\LINEBot\Event('user');
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->replyMessage($event->getReplyToken(), $textMessageBuilder);
 if ($response->isSucceeded()) {
